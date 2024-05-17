f9b6345f9112b81dd7dc0db4e496a26d71ad2352
wakinghours <wakinghoursszu@outlook.com>
2024-05-17 19:54:21 +0800

    EPL

--- backbone.py
+++ backbone.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from head import *
-from head_VPCL import *
+from head_EPL import *
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1):
     """3x3 convolution with padding"""
